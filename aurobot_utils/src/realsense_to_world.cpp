d73b1527cd3da3031b19a6d7974cfe0f7fed15df
yayan <brayan.inf@hotmail.com>
2017-05-18 11:46:58 +0200

    Changed the orientation of the aurobot head_link to fit the camera view. Added a new package called "aurobot_utils". It holds now the script for transforming the camera point cloud topic to the world frame.

---
+++ aurobot_utils/src/realsense_to_world.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+
+//#include <pcl_conversions/pcl_conversions.h>
+
+#include <tf/transform_listener.h>
+#include <pcl_ros/transforms.h>
+
+
+ros::Publisher pub;
+
+void cloudCallback(const sensor_msgs::PointCloud2ConstPtr & inputCloudMsg) {
+  sensor_msgs::PointCloud2 outputCloudMsg;
+  tf::TransformListener tfListener;
+  tf::StampedTransform transform;
+
+  tfListener.waitForTransform("/world", "/head_link", ros::Time(0), ros::Duration(3.0));
+  tfListener.lookupTransform("/world", "/head_link", ros::Time(0), transform);
+
+  pcl_ros::transformPointCloud("/world", transform, *inputCloudMsg, outputCloudMsg);
+
+  pub.publish (outputCloudMsg);
+
+  ROS_INFO_NAMED("mapper", "New RealSense cloud mapped");
+}
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "realsense_to_world_mapper");
+
+  ros::NodeHandle n;
+  ros::NodeHandle nh;
+  ros::Subscriber sub = n.subscribe<sensor_msgs::PointCloud2>("/camera/depth_registered/points",
+    1, cloudCallback);
+ 
+  pub = nh.advertise<sensor_msgs::PointCloud2>("/camera/depth_registered/points_tfed", 30);
+
+  ros::spin();
+
+  return 0;
+}
